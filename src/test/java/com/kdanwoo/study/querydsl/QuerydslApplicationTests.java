c4736fd21e2ce96d3db47f85c932b970b556631a
kdanwoo <k.danwoo91@gmail.com>
2021-10-11 17:39:05 +0900

    프로젝트 실습 환경 구성

--- src/test/java/com/kdanwoo/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/kdanwoo/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,39 @@
 package com.kdanwoo.study.querydsl;
 
+import com.kdanwoo.study.querydsl.entity.Hello;
+import com.kdanwoo.study.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
+
+        Hello result = queryFactory.selectFrom(qHello).fetchOne();
+
+        Assertions.assertThat(result).isEqualTo(hello);
+
+        //lombok 동작확인
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
