25017cd710c9148557312c9d15dcb0e63d155c15
kdanwoo <k.danwoo91@gmail.com>
2021-10-13 21:17:17 +0900

    querydsl 문법

--- src/test/java/com/kdanwoo/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/kdanwoo/study/querydsl/QuerydslBasicTest.java
@@ -145,7 +145,7 @@ public class QuerydslBasicTest {
         List<Member> fetch = queryFactory.selectFrom(member).fetch();
 
         //단 건 엔티티 객체로 조회한다
-        Member member = queryFactory.selectFrom(QMember.member).fetchOne();
+        //Member member = queryFactory.selectFrom(QMember.member).fetchOne();
 
         //처음 한 건 조회
         Member memberFirst = queryFactory.selectFrom(QMember.member).fetchFirst();
@@ -153,7 +153,9 @@ public class QuerydslBasicTest {
         //페이징에서 사용
         QueryResults<Member> results = queryFactory
                 .selectFrom(QMember.member)
-                .fetchResults();
+                .fetchResults(); //쿼리가 2번 나감, totalcount 때문에
+
+        System.out.println("results = " + results.getResults());
 
         //count 쿼리로 변경
         long count = queryFactory
@@ -161,4 +163,32 @@ public class QuerydslBasicTest {
                 .fetchCount();
 
     }
+
+    /**
+     *회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     * */
+    @Test
+    public void sortTest(){
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+
 }
